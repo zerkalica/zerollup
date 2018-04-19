7121e97e40a435070b735cde5c785756bef1a12d
Stefan Zerkalica <zerkalica@gmail.com>
2018-04-20 00:17:35 +0300

    alias order fix

--- packages/preset-ts/src/index.ts
+++ packages/preset-ts/src/index.ts
@@ -80,7 +80,6 @@ export default function rollupConfig(
                     pkgRoot: pkg.pkgRoot,
                     isLib: pkg.lib
                 }),
-                alias(aliases),
                 typescript({
                     abortOnError: true,
                     check: !watch,
@@ -93,6 +92,7 @@ export default function rollupConfig(
                         }
                     }
                 }),
+                alias(aliases),
                 ...commonPlugins,
             ]
 
