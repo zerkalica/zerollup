e22cc907a7ae84c1a0825ad057c05af7a4b68ecc
Stefan Zerkalica <zerkalica@gmail.com>
2018-04-23 15:24:28 +0300

    trasform plugin cache

--- packages/ts-transform-paths/src/index.ts
+++ packages/ts-transform-paths/src/index.ts
@@ -2,15 +2,22 @@ import * as path from 'path'
 import * as ts from 'typescript'
 import {ImportPathsResolver, createTraverseVisitor} from '@zerollup/ts-helpers'
 
+interface ModifiedExpression {
+    target: ts.Expression
+    oldPos: number
+    oldEnd: number
+}
+
 interface ImportPathVisitorContext {
     resolver: ImportPathsResolver
     posMap: Map<string, number>
+    expressions: ModifiedExpression[]
 }
 
 const importPathRegex = /^(['"\s]+)(.+)(['"\s]+)$/
-const commentPrefix = '\n//'
+const commentPrefix = '\n// ###'
 
-function importPathVisitor(node: ts.Node, {posMap, resolver}: ImportPathVisitorContext): ts.Node | void {
+function importPathVisitor(node: ts.Node, {expressions, posMap, resolver}: ImportPathVisitorContext): ts.Node | void {
     if (!ts.isImportDeclaration(node) && !ts.isExportDeclaration(node)) return
 
     const moduleSpecifier = node.moduleSpecifier
@@ -44,37 +51,65 @@ function importPathVisitor(node: ts.Node, {posMap, resolver}: ImportPathVisitorC
         sf.text += strToAdd
         sf.end += strToAdd.length
     }
+    expressions.push({
+        target: moduleSpecifier,
+        oldPos: moduleSpecifier.pos,
+        oldEnd: moduleSpecifier.end
+    })
+
     moduleSpecifier.pos = cachedPos
     moduleSpecifier.end = moduleSpecifier.pos + newStr.length
+}
 
-    const newSpec = ts.createLiteral(newImport)
-
-    if (ts.isImportDeclaration(node)) return ts.updateImportDeclaration(
-        node, undefined, undefined, undefined, newSpec
-    )
-
-    if (ts.isExportDeclaration(node)) return ts.updateExportDeclaration(
-        node, undefined, undefined, undefined, newSpec
-    )
+interface CacheRec {
+    originalText: string
+    expressions: ModifiedExpression[]
 }
 
 export default function transformPaths(ls: ts.LanguageService) {
+    const cache = new Map <string, CacheRec>()
+
     return {
         before(transformationContext: ts.TransformationContext): ts.Transformer<ts.SourceFile> {
             const resolver = new ImportPathsResolver(transformationContext.getCompilerOptions())
 
             return (sf: ts.SourceFile) => {
-                const importPathVisitorContext: ImportPathVisitorContext = {
+                const expressions: ModifiedExpression[] = []
+                const originalText = sf.text
+                const ctx: ImportPathVisitorContext = {
                     resolver,
-                    posMap: new Map()
+                    posMap: new Map<string, number>(),
+                    expressions
                 }
+
                 const visitor = createTraverseVisitor(
                     importPathVisitor,
-                    importPathVisitorContext,
+                    ctx,
                     transformationContext
                 )
 
-                return ts.visitNode(sf, visitor)
+                const newSf = ts.visitNode(sf, visitor)
+                if (ctx.expressions.length === 0) return sf
+
+                cache.set(sf.fileName, {originalText, expressions})
+
+                return newSf
+            }
+        },
+        after(transformationContext: ts.TransformationContext): ts.Transformer<ts.SourceFile> {
+            return (sf: ts.SourceFile) => {
+                const rec = cache.get(sf.fileName)
+                if (!rec) return sf
+                for (let expr of rec.expressions) {
+                    expr.target.pos = expr.oldPos
+                    expr.target.end = expr.oldEnd
+                }
+
+                sf.text = rec.originalText
+
+                cache.delete(sf.fileName)
+
+                return sf
             }
         }
     }
