ef0803aecd5f5bb1116e1bbed00662f8a872be10
Stefan Zerkalica <zerkalica@gmail.com>
2018-05-18 14:04:50 +0300

    fix(build): set upstream removed

--- .ci/lerna.sh
+++ .ci/lerna.sh
@@ -9,8 +9,6 @@ git config --global user.name "Marty Mcfly"
 echo "setup remote for $(git config --get remote.origin.url) $BRANCH"
 git remote add ci ${REPOSITORY}
 
-git branch --set-upstream-to=ci/$BRANCH $BRANCH
-
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
 for package in ./packages/*/; do
